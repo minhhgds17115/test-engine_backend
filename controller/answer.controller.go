956e16b5b11a2a619978761d60a0e161d83b8101
minhhgds17115 <82482747+minhhgds17115@users.noreply.github.com>
2022-08-31 00:50:03 +0700

    add api for  question, answer

--- controller/answer.controller.go
+++ controller/answer.controller.go
@@ -1,15 +1,28 @@
 package controller
 
 import (
+	"net/http"
+
 	"example.com/m/v2/services"
+	"github.com/gin-gonic/gin"
 )
 
 type AnswerController struct {
-	questionService *services.AnswerServiceImpl
+	answerService *services.AnswerServiceImpl
 }
 
 func NewAnswerController(answerService *services.AnswerServiceImpl) *AnswerController {
 	return &AnswerController{
-		// answerService: answerService,
+		answerService: answerService,
 	}
+}
+
+func (uc *AnswerController) getAnswer(ctx *gin.Context) {
+	var Answerid string = ctx.Param("Answerid")
+	user, err := uc.answerService.getAnswer(Answerid)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
